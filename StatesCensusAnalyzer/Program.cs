96a176836c8e042d1f06226bc67720d76118b1d3
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-27 09:24:41 +0530

    First commit

---
+++ StatesCensusAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatesCensusAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to States Census Analyzer Demo!");
+        }
+    }
+}
